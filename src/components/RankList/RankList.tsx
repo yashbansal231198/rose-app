b8f6cc207fcc5539a4e24c4fea648e9165685a8b
Yash Bansal <yashbansal4953@gmail.com>
2021-08-13 17:10:43 +0530

    custom hooks and types

--- src/components/RankList/RankList.tsx
+++ src/components/RankList/RankList.tsx
@@ -1,7 +1,4 @@
 import React from "react";
-import ryan from "../../assets/ryan.jpeg";
-import joel from "../../assets/joel.jpeg";
-import vansh from "../../assets/vansh.jpeg";
 import _2nd from "../../assets/2nd.png";
 import _3rd from "../../assets/3rd.png";
 import crown from "../../assets/crown.png";
@@ -9,29 +6,10 @@ import stars1 from "../../assets/stars1.png";
 import stars2 from "../../assets/stars2.png";
 import stars3 from "../../assets/stars3.png";
 import coins from "../../assets/coins.png";
-import default_profile from "../../assets/default_profile.png";
-import profile2 from "../../assets/profile2.png";
-import profile3 from "../../assets/profile3.png";
-import profile4 from "../../assets/profile4.png";
-import profile5 from "../../assets/profile5.png";
 import { List, AutoSizer } from "react-virtualized";
 import Divider from "@material-ui/core/Divider";
 import useMembers from "../../hooks/useMembers";
 import MemberImages from "./MemberImages";
-let func = (index: number) => {
-  if (index % 5 == 0) return `${default_profile}`;
-  if (index % 5 == 1) return `${profile2}`;
-  if (index % 5 == 2) return `${profile3}`;
-  if (index % 5 == 3) return `${profile4}`;
-  if (index % 5 == 4) return `${profile5}`;
-};
-let func2 = (index: number) => {
-  if (index % 5 == 0) return "Sarthak Saxena";
-  if (index % 5 == 1) return "Rachit Pathak";
-  if (index % 5 == 2) return "Aayush Vashist";
-  if (index % 5 == 3) return "Archana Linson";
-  if (index % 5 == 4) return "Rishabh Mittal";
-};
 function RankList() {
   const members = useMembers();
   return (
@@ -42,7 +20,7 @@ function RankList() {
           <div className="featured">
             <div className="leader1">
               <img className="position" src={_2nd} />
-              <img className="featured1" src={ryan} />
+              <img className="featured1" src={MemberImages[0]} />
               <div className="box">
                 <img className="star" src={stars1} />
                 <span>{members[0].coin}</span>
@@ -52,7 +30,7 @@ function RankList() {
             </div>
             <div className="leader2">
               <img className="position1" src={crown} />
-              <img className="featured2" src={joel} />
+              <img className="featured2" src={MemberImages[1]} />
               <div className="box">
                 <img className="star" src={stars1} />
                 <span>{members[1].coin}</span>
@@ -62,7 +40,7 @@ function RankList() {
             </div>
             <div className="leader1">
               <img className="position" src={_3rd} />
-              <img className="featured1" src={vansh} />
+              <img className="featured1" src={MemberImages[2]} />
               <div className="box">
                 <img className="star" src={stars1} />
                 <span>{members[2].coin}</span>
